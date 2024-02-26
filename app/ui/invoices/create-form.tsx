6fa109539a35dedd6da8731a0f9bdcc85f2a03f1
Blake.Liu <Blake.Liu@hualiteq.com>
2024-02-26 13:45:50 +0800

    ch12 mutate data

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,7 +7,7 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import createInvoice from '@/app/lib/action';
+import { createInvoice } from '@/app/lib/action';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
